84ad0e5ade1378a1154da0c4b04684e0b4539ac9
Daniel Dalby <danieldalby@Daniels-MacBook-Pro-2.local>
2020-05-15 00:40:01 +0200

    test med Dr. Fill

--- test.js
+++ test.js
@@ -35,7 +35,15 @@ function visPaintings() {
 
 
 
+window.addEventListener("scroll", fillBar)
 
+
+function fillBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.querySelector("#myBar").style.width = scrolled + "%";
+}
 
 
 
