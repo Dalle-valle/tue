1ee5f4e92771a298aa010775413cab0ef7b749f9
Daniel Dalby <danieldalby@Daniels-MacBook-Pro-2.local>
2020-05-26 00:23:20 +0200

    fixes

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ function fillBar() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.querySelector("#myBar").style.width = scrolled + "%";
+    document.querySelector(".progress-bar").style.width = scrolled + "%";
+
 }
