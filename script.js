6ed25f64fcb547586d5224e148b94a6af8b40640
Daniel Dalby <danieldalby@Daniels-MBP-2.parknet.dk>
2020-06-05 02:26:56 +0200

    lort

--- script.js
+++ script.js
@@ -15,21 +15,31 @@ function fillBar() {
 document.querySelector(".burger_menu").addEventListener("click", burger);
 
 
+
+
+const fading = document.querySelector(".menu_wrapper");
+
 function burger() {
     const bmenu = document.querySelector(".mylinks");
-    fade();
+
 
 
     if (bmenu.style.display === "flex") {
         bmenu.style.display = "none";
-        document.querySelector(".menu_wrapper").classList.add("fadein");
-        document.querySelector(".menu_wrapper").classList.remove("fadein");
+
 
 
         document.querySelector(".burger_menu img").src = "billeder/burger.svg";
         document.querySelector(".c_baskin").style.visibility = "visible";
         document.querySelector("#progress").style.visibility = "visible";
         document.querySelector(".logo").style.visibility = "visible";
+        fading.classList.add("fadeout")
+        fading.addEventListener("animationend", () => {
+            fading.classList.remove("fadeout");
+        })
+
+
+
 
 
 
@@ -39,23 +49,14 @@ function burger() {
         document.querySelector(".c_baskin").style.visibility = "hidden";
         document.querySelector("#progress").style.visibility = "hidden";
         document.querySelector(".logo").style.visibility = "hidden";
-
-        fade();
-
-
+        fading.classList.add("fadein")
+        fading.addEventListener("animationend", () => {
+            fading.classList.remove("fadein");
+        })
 
 
 
 
 
     }
-}
-
-function fade() {
-    if (document.querySelector(".menu_wrapper").classList.contains("fadeout")) {
-        document.querySelector(".menu_wrapper").classList.add("fadein")
-    } else {
-        document.querySelector(".menu_wrapper").classList.add("fadein");
-    }
-
 }
